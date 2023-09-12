53cfad60417269b60ea483b6bffe31aa9ec5a7bb
Salim Khiter <khiter.salim@gmail.com>
2023-09-12 23:06:05 +0200

    feat(i18n): update version

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,15 @@
 import "@angular/localize/init";
 import { loadTranslations } from "@angular/localize";
-import { getLocale, withLocale, useOnDocument, $ } from "@builder.io/qwik";
+import { getLocale, withLocale } from "@builder.io/qwik";
 import type { RenderOptions } from "@builder.io/qwik/server";
 
 // You must declare all your locales here
-import EN from "./locale/message.en.json";
-import FR from "./locale/message.fr.json";
+import EN from "./locales/message.en.json";
+import IT from "./locales/message.it.json";
+import FR from "./locales/message.fr.json";
 
 // Make sure it's obvious when the default locale was selected
-const defaultLocale = "en";
-
-/**
- * Function used to load all translations variants.
- */
-export function initTranslations() {
-  console.log("  ➜  Loading translations...");
-  [EN, FR].forEach(({ translations, locale }) => {
-    // withLocale sets the locale for the duration of the callback
-    withLocale(locale, () => loadTranslations(translations));
-  });
-}
+const DEFAULT_LOCALE = "fr";
 
 /**
  * This file is left for the developer to customize to get the behavior they want for localization.
@@ -35,60 +25,43 @@ const $localizeFn = $localize as any as {
  * This solution uses the `@angular/localize` package for translations, however out of the box
  * `$localize` works with a single translation only. This code adds support for multiple locales
  * concurrently. It does this by intercepting the `TRANSLATIONS` property read and returning
- * appropriate translation based on the current locale.
+ * appropriate translation based on the current locale which is store in the `usEnvDate('local')`.
  */
+
 if (!$localizeFn.TRANSLATION_BY_LOCALE) {
   $localizeFn.TRANSLATION_BY_LOCALE = new Map([["", {}]]);
-  Object.defineProperty($localizeFn, "TRANSLATIONS", {
+  Object.defineProperty($localize, "TRANSLATIONS", {
     get: function () {
-      const locale = getLocale();
-      let translations = this.TRANSLATION_BY_LOCALE.get(locale);
+      const locale = getLocale(DEFAULT_LOCALE);
+      let translations = $localizeFn.TRANSLATION_BY_LOCALE.get(locale);
       if (!translations) {
-        this.TRANSLATION_BY_LOCALE.set(locale, (translations = {}));
+        $localizeFn.TRANSLATION_BY_LOCALE.set(locale, (translations = {}));
       }
       return translations;
     },
   });
 }
 
-const validateLocale = (locale: string) => {
-  if ($localizeFn.TRANSLATION_BY_LOCALE.has(locale)) return locale;
-  const match = /^([^-;]+)[-;]/.exec(locale);
-  return (
-    (match && $localizeFn.TRANSLATION_BY_LOCALE.has(match[1]) && match[1]) ||
-    undefined
-  );
-};
+/**
+ * Function used to load all translations variants.
+ */
+export function initTranslations() {
+  [EN, IT, FR].forEach(({ translations, locale }) => {
+    withLocale(locale, () => loadTranslations(translations));
+  });
+}
 
 /**
  * Function used to examine the request and determine the locale to use.
  *
- * in this implementation, we accept a `?locale=xx` parameter to override
- * the auto-detected locale requested by the browser.
+ * This function is meant to be used with `RenderOptions.locale` property
  *
- * This function is meant to be used with `RenderOptions.locale` property.
- * It must always return a valid locale so that prod clients will always get de-$localize-d js
- *
- * @returns The locale to use, which will be stored in the render context.
+ * @returns The locale to use which will be stored in the `useEnvData('locale')`.
  */
-export function extractLang(request: Request, url: URL): string {
-  // This is not really needed because we handle /locale, but it's here as an example
-  let locale = url.searchParams.get("locale") || undefined;
-  if (locale) {
-    // note that we mutate the URL here, this will update the search property
-    url.searchParams.delete("locale");
-    locale = validateLocale(locale);
-    if (locale) return locale;
-  }
-  // Parse the browser accept-language header
-  const locales = request.headers.get("accept-language")?.split(",");
-  if (locales)
-    for (const entry of locales) {
-      locale = validateLocale(entry);
-      if (locale) return locale;
-    }
-
-  return defaultLocale;
+export function extractLang(locale: string): string {
+  return locale && $localizeFn.TRANSLATION_BY_LOCALE.has(locale)
+    ? locale
+    : DEFAULT_LOCALE;
 }
 
 /**
@@ -109,12 +82,15 @@ export function extractBase({ serverData }: RenderOptions): string {
 }
 
 export function useI18n() {
+  // Runtime translation is used during development only.
   if (import.meta.env.DEV) {
-    // During development only, load all translations in memory when the app starts on the client.
-    // eslint-disable-next-line qwik/use-method-usage
-    useOnDocument("qinit", $(initTranslations));
+    return initTranslations;
   }
+  // Otherwise, will return a noop
+  return () => {};
 }
 
 // We always need the translations on the server
-if (import.meta.env.SSR) initTranslations();
+if (import.meta.env.SSR) {
+  initTranslations();
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 import { extractLang } from "~/i18n";
 
-export const onGet: RequestHandler = async ({ request, redirect, url }) => {
-  const guessedLocale = extractLang(request, url);
+export const onGet: RequestHandler = async ({ redirect, url, params }) => {
+  const guessedLocale = extractLang(params.lang);
   console.log(`  ➜  GET / - Redirecting to /${guessedLocale}...`);
   throw redirect(301, `/${guessedLocale}/${url.search}`);
 };
